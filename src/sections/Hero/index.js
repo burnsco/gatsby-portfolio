dd8ffd0ae85b364f1d7768b776e7a4ebe8674982
Corey Burns <coreymburns@gmail.com>
2020-07-31 18:28:06 -0400

    removing gatsby img on hero for faster load?

--- src/sections/Hero/index.js
+++ src/sections/Hero/index.js
@@ -1,70 +1,50 @@
 /** @jsx jsx */
-import { jsx, Flex, Heading } from 'theme-ui'
-import Img from 'gatsby-image/withIEPolyfill'
+import { jsx, Flex, Heading, Image } from 'theme-ui'
 import PropTypes from 'prop-types'
-import { graphql, useStaticQuery } from 'gatsby'
+import avatar from '../../data/images/Me.jpg'
 import SectionContainer from '../Container/SectionContainer'
 import SocialIcons from '../../components/SocialIcons'
 import AnimatedWave from '../../components/AnimatedWave'
 import Header from '../../components/Header'
 
-const Hero = ({ title }) => {
-  const { data } = useStaticQuery(graphql`
-    {
-      data: allHeroJson {
-        nodes {
-          display
-          subDisplay
-          image {
-            childImageSharp {
-              fixed(width: 130, height: 130) {
-                ...GatsbyImageSharpFixed_withWebp
-              }
-            }
-          }
-        }
-      }
-    }
-  `)
-  return (
-    <SectionContainer id={title} aria-label={title}>
-      <Header />
-      <Flex
+const Hero = ({ title }) => (
+  <SectionContainer id={title} aria-label={title}>
+    <Header />
+    <Flex
+      sx={{
+        background: theme => [
+          `${theme.colors.eerieblack}`,
+          `linear-gradient(${theme.colors.primary}, ${theme.colors.secondary})`,
+        ],
+        minHeight: '95vh',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Image
         sx={{
-          background: theme => [
-            `${theme.colors.eerieblack}`,
-            `linear-gradient(${theme.colors.primary}, ${theme.colors.secondary})`,
-          ],
-          minHeight: '95vh',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
+          height: 130,
+          width: 130,
+          mb: 3,
+          objectFit: 'cover',
+          objectPosition: '50% 50%',
+          WebkitBoxShadow:
+            '0 0 1px 11px rgba(80, 81, 79, .15), 0 0 1px 22px rgba(80, 81, 79, .1)',
+          boxShadow:
+            '0 0 1px 11px rgba(80, 81, 79, .15), 0 0 1px 22px rgba(80, 81, 79, .1)',
+          borderRadius: '9999px',
+          transform: `rotate(-35deg)`,
         }}
-      >
-        <Img
-          objectFit="cover"
-          objectPosition="50% 50%"
-          sx={{
-            mb: 3,
-            WebkitBoxShadow:
-              '0 0 1px 11px rgba(80, 81, 79, .15), 0 0 1px 22px rgba(80, 81, 79, .1)',
-            boxShadow:
-              '0 0 1px 11px rgba(80, 81, 79, .15), 0 0 1px 22px rgba(80, 81, 79, .1)',
-            borderRadius: '9999px',
-            transform: `rotate(-35deg)`,
-          }}
-          fixed={data.nodes[0].image.childImageSharp.fixed}
-        />
-        <Heading variant="textStyles.display">{data.nodes[0].display}</Heading>
-        <Heading variant="textStyles.subDisplay">
-          {data.nodes[0].subDisplay}
-        </Heading>
-        <SocialIcons />
-        <AnimatedWave enableWave bottom />
-      </Flex>
-    </SectionContainer>
-  )
-}
+        src={avatar}
+      />
+      <Heading variant="textStyles.display">Corey Burns</Heading>
+      <Heading variant="textStyles.subDisplay">Full-stack Developer</Heading>
+      <SocialIcons />
+      <AnimatedWave enableWave bottom />
+    </Flex>
+  </SectionContainer>
+)
 
 Hero.propTypes = {
   title: PropTypes.string.isRequired,
